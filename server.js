6a83dd034075d004333f3e5bc4a8ab89c17e5f8b
madedev <nmaderama@gmail.com>
2025-03-13 11:14:32 +0700

    Solve Failed Login 2

--- server.js
+++ server.js
@@ -29,13 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*',
     credentials: true,
   })
 );
